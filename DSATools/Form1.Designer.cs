0819f2d55093bf4419da2fa8e70c408b5fd7c382
Jonas Schmidt <jonas.schmidt1404@gmail.com>
2020-05-07 16:02:57 +0200

    Funktionalität fertiggestellt

--- DSATools/Form1.Designer.cs
+++ DSATools/Form1.Designer.cs
@@ -28,13 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bBerechnen = new System.Windows.Forms.Button();
+            this.lBSpalte = new System.Windows.Forms.ListBox();
+            this.nUDStartwert = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nUDEndwert = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tBAPPreis = new System.Windows.Forms.TextBox();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.bTabelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDStartwert)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDEndwert)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bBerechnen
+            // 
+            this.bBerechnen.Enabled = false;
+            this.bBerechnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBerechnen.Location = new System.Drawing.Point(227, 200);
+            this.bBerechnen.Name = "bBerechnen";
+            this.bBerechnen.Size = new System.Drawing.Size(164, 121);
+            this.bBerechnen.TabIndex = 0;
+            this.bBerechnen.Text = "Berechnen";
+            this.bBerechnen.UseVisualStyleBackColor = true;
+            this.bBerechnen.Click += new System.EventHandler(this.bBerechnen_Click);
+            // 
+            // lBSpalte
+            // 
+            this.lBSpalte.FormattingEnabled = true;
+            this.lBSpalte.Items.AddRange(new object[] {
+            "A*",
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H"});
+            this.lBSpalte.Location = new System.Drawing.Point(12, 41);
+            this.lBSpalte.Name = "lBSpalte";
+            this.lBSpalte.Size = new System.Drawing.Size(120, 121);
+            this.lBSpalte.TabIndex = 1;
+            this.lBSpalte.SelectedIndexChanged += new System.EventHandler(this.lBSpalte_SelectedIndexChanged);
+            // 
+            // nUDStartwert
+            // 
+            this.nUDStartwert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nUDStartwert.Location = new System.Drawing.Point(138, 93);
+            this.nUDStartwert.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nUDStartwert.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.nUDStartwert.Name = "nUDStartwert";
+            this.nUDStartwert.Size = new System.Drawing.Size(120, 29);
+            this.nUDStartwert.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Spalte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Aktuell";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(267, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Endwert";
+            // 
+            // nUDEndwert
+            // 
+            this.nUDEndwert.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nUDEndwert.Location = new System.Drawing.Point(264, 93);
+            this.nUDEndwert.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.nUDEndwert.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nUDEndwert.Name = "nUDEndwert";
+            this.nUDEndwert.Size = new System.Drawing.Size(120, 29);
+            this.nUDEndwert.TabIndex = 7;
+            this.nUDEndwert.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "AP Preis";
+            // 
+            // tBAPPreis
+            // 
+            this.tBAPPreis.BackColor = System.Drawing.SystemColors.Control;
+            this.tBAPPreis.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tBAPPreis.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.tBAPPreis.Location = new System.Drawing.Point(12, 277);
+            this.tBAPPreis.Name = "tBAPPreis";
+            this.tBAPPreis.ReadOnly = true;
+            this.tBAPPreis.Size = new System.Drawing.Size(202, 37);
+            this.tBAPPreis.TabIndex = 10;
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.DefaultExt = "csv";
+            this.OpenFileDialog.FileName = "SteigerungsTabelle";
+            this.OpenFileDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            this.OpenFileDialog.Title = "Datei öffnen";
+            // 
+            // bTabelle
+            // 
+            this.bTabelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bTabelle.Location = new System.Drawing.Point(227, 128);
+            this.bTabelle.Name = "bTabelle";
+            this.bTabelle.Size = new System.Drawing.Size(164, 66);
+            this.bTabelle.TabIndex = 11;
+            this.bTabelle.Text = "Tabelle Laden";
+            this.bTabelle.UseVisualStyleBackColor = true;
+            this.bTabelle.Click += new System.EventHandler(this.bTabelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(403, 333);
+            this.Controls.Add(this.bTabelle);
+            this.Controls.Add(this.tBAPPreis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nUDEndwert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nUDStartwert);
+            this.Controls.Add(this.lBSpalte);
+            this.Controls.Add(this.bBerechnen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "AP Rechner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nUDStartwert)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDEndwert)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bBerechnen;
+        private System.Windows.Forms.ListBox lBSpalte;
+        private System.Windows.Forms.NumericUpDown nUDStartwert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nUDEndwert;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tBAPPreis;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button bTabelle;
     }
 }
 
--- DSATools/Form1.cs
+++ DSATools/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,167 @@ namespace DSATools
 {
     public partial class Form1 : Form
     {
+        string spalte = "A";
+        string path = null;
+        string[] steigerungskosten = { "" };
+
+        bool fileSelected = false;
+
         public Form1()
         {
             InitializeComponent();
+            lBSpalte.SelectedIndex = 0;
+            spalte = lBSpalte.Text;
+        }
+
+        private void bBerechnen_Click(object sender, EventArgs e)
+        {
+            APBerechnen();
+        }
+
+        public void APBerechnen()
+        {
+            SteigerungsTabelleEinlesen(spalte);
+        }
+
+        private void SteigerungsTabelleEinlesen(string spalte)
+        {
+            int zeilenNr = 1;
+            StreamReader Daten = null;
+
+            int gesuchteZeile = 0;
+
+            Console.WriteLine(spalte);
+            if (spalte == "A*")
+            {
+                gesuchteZeile = 1;
+            }
+            else if (spalte == "A")
+            {
+                gesuchteZeile = 2;
+            }
+            else if (spalte == "B")
+            {
+                gesuchteZeile = 3;
+            }
+            else if (spalte == "C")
+            {
+                gesuchteZeile = 4;
+            }
+            else if (spalte == "D")
+            {
+                gesuchteZeile = 5;
+            }
+            else if (spalte == "E")
+            {
+                gesuchteZeile = 6;
+            }
+            else if (spalte == "F")
+            {
+                gesuchteZeile = 7;
+            }
+            else if (spalte == "G")
+            {
+                gesuchteZeile = 8;
+            }
+            else if (spalte == "H")
+            {
+                gesuchteZeile = 9;
+            }
+            if (path != null)
+            {
+                try
+                {
+                    Daten = new StreamReader(path);
+
+                    // bis Dateiende einlesen
+                    while (!Daten.EndOfStream)
+                    {
+                        Console.WriteLine(zeilenNr);
+                        Console.WriteLine(gesuchteZeile);
+                        if (zeilenNr != gesuchteZeile)
+                        {
+                            Daten.ReadLine();
+                        }
+                        else
+                        {
+                            string eineZeile = Daten.ReadLine();
+                            // Semikolon am Ende der Zeile ggf. löschen
+                            if (eineZeile[eineZeile.Length - 1] == ';')
+                                eineZeile = eineZeile.Remove(eineZeile.Length - 1);
+                            // eingelesene Zeile aufsplitten
+                            steigerungskosten = eineZeile.Split(';');
+                            Console.WriteLine(steigerungskosten);
+                            fileSelected = true;
+                            //Daten des Halters einlesen
+                        }
+                        //Nächste Zeile
+                        zeilenNr++;
+                    }
+
+
+                }
+                catch (Exception Ex)
+                {
+                    //Fehler abfangen
+                    MessageBox.Show(String.Format("Es ist ein Fehler bei Einlesen der Datei aufgetreten: {0} bei Zeile {1}", Ex.Message, zeilenNr));
+                }
+                finally
+                {
+                    if (Daten != null)
+                        Daten.Close();
+                }
+                int i = 0;
+
+                if (nUDStartwert.Value < -1)
+                {
+                    i = 0;
+                }
+                else { i = Convert.ToInt32(nUDStartwert.Value + 1); }
+
+                int Ap = 0;
+                while (i <= nUDEndwert.Value)
+                {
+                    
+                    if (i <= 31)
+                    { 
+                        Ap += Convert.ToInt32(steigerungskosten[i]);
+                        Console.WriteLine(i + " Steigerungskosten = " + steigerungskosten[i]);
+                    } else if(i > 31)
+                    {
+                        Ap += Convert.ToInt32(steigerungskosten[31]);
+                        Console.WriteLine(i + " Steigerungskosten = " + steigerungskosten[31]);
+                    }
+                    i++;
+                }
+                tBAPPreis.Text = Ap.ToString();
+            }
+
+
+
+
+        }
+
+        private void lBSpalte_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            spalte = lBSpalte.Text;
+            Console.WriteLine(spalte);
+        }
+
+        private void bTabelle_Click(object sender, EventArgs e)
+        {
+
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+
+            {
+                path = OpenFileDialog.FileName;
+                bBerechnen.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
